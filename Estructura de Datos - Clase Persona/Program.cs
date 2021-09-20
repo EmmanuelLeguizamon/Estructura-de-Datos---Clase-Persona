69b5d2adf0d4f010f307c87623fdbab1584b011e
EmmanuelLeguizamon <ema.legui.carp@gmail.com>
2021-09-20 19:59:31 -0300

    Agregar archivos de proyecto.

---
+++ Estructura de Datos - Clase Persona/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estructura_de_Datos___Clase_Persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
